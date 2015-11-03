0f742bb317e3633e1ad43753b5474d9a5a236326
Isak Sky <isak.sky@gmail.com>
2015-11-02 23:36:13 -0700

    clean cruft

--- src/RouteProvider/TpUtils.fs
+++ src/RouteProvider/TpUtils.fs
@@ -2,22 +2,6 @@
 
 open System
 
-let handlerFuncTypeDef = 
-  function 
-  | 0 ->  typedefof<Action>
-  | 1 ->  typedefof<Action<_>>
-  | 2 ->  typedefof<Action<_, _>>
-  | 3 ->  typedefof<Action<_, _, _>>
-  | 4 ->  typedefof<Action<_, _, _, _>>
-  | 5 ->  typedefof<Action<_, _, _, _, _>>
-  | 6 ->  typedefof<Action<_, _, _, _, _, _>>
-  | 7 ->  typedefof<Action<_, _, _, _, _, _, _>>
-  | 8 ->  typedefof<Action<_, _, _, _, _, _, _, _>>
-  | 9 ->  typedefof<Action<_, _, _, _, _, _, _, _, _>>
-  | 10 -> typedefof<Action<_, _, _, _, _, _, _, _, _, _>>
-  | 11 -> typedefof<Action<_, _, _, _, _, _, _, _, _, _, _>>
-  | i ->  failwithf "arity %d too high" i
-
 type internal UriResolutionType = 
   | DesignTime
   | Runtime
@@ -59,46 +43,3 @@ type internal UriResolver =
       | RuntimeInFSI -> x.DefaultResolutionFolder
       | Runtime -> AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\', '/')
     Uri(System.IO.Path.Combine(root, uri.OriginalString), UriKind.Absolute)
-
-module QuotationHelpers = 
-  open Microsoft.FSharp.Quotations
-  open Microsoft.FSharp.Reflection
-  
-  let rec coerceValues fieldTypeLookup fields = 
-    Array.mapi (fun i v -> 
-      let expr = 
-        if v = null then simpleTypeExpr v
-        elif FSharpType.IsUnion(v.GetType()) then unionExpr v |> snd
-        elif FSharpType.IsRecord(v.GetType()) then recordExpr v |> snd
-        else simpleTypeExpr v
-      Expr.Coerce(expr, fieldTypeLookup i)) fields
-    |> List.ofArray
-  
-  and simpleTypeExpr instance = Expr.Value(instance)
-  
-  and unionExpr instance = 
-    let caseInfo, fields = FSharpValue.GetUnionFields(instance, instance.GetType())
-    let fieldInfo = caseInfo.GetFields()
-    let fieldTypeLookup indx = fieldInfo.[indx].PropertyType
-    caseInfo.DeclaringType, Expr.NewUnionCase(caseInfo, coerceValues fieldTypeLookup fields)
-  
-  and recordExpr instance = 
-    let tpy = instance.GetType()
-    let fields = FSharpValue.GetRecordFields(instance)
-    let fieldInfo = FSharpType.GetRecordFields(tpy)
-    let fieldTypeLookup indx = fieldInfo.[indx].PropertyType
-    tpy, Expr.NewRecord(instance.GetType(), coerceValues fieldTypeLookup fields)
-  
-  and arrayExpr (instance : 'a array) = 
-    let typ = typeof<'a>
-    let arrayType = instance.GetType()
-    let exprs = coerceValues (fun _ -> typ) (instance |> Array.map box)
-    arrayType, Expr.NewArray(typ, exprs)
-  
-  let createLetExpr varType instance body args = 
-    let var = Var("instance", varType)
-    Expr.Let(var, instance, body args (Expr.Var(var)))
-  
-  let quoteUnion instance = unionExpr instance ||> createLetExpr
-  let quoteRecord instance = recordExpr instance ||> createLetExpr
-  let quoteArray instance = arrayExpr instance ||> createLetExpr
