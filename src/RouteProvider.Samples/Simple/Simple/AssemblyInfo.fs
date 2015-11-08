75001d31e02fb26b96e2842080ef91853e03014c
Isak Sky <isak.sky@gmail.com>
2015-11-07 17:02:58 -0700

    add simple test project

---
+++ src/RouteProvider.Samples/Simple/Simple/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Simple.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Simple")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Microsoft")>]
+[<assembly: AssemblyProduct("Simple")>]
+[<assembly: AssemblyCopyright("Copyright © Microsoft 2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("e8c9ce57-5df0-4b88-8290-9fe2318aa28d")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ src/RouteProvider.Samples/Simple/Simple/Simple.fsx
@@ -0,0 +1,27 @@
+﻿#r @"..\..\..\RouteProvider\bin\Debug\RouteProvider.dll"
+
+[<Literal>]
+let routes = """
+  GET projects/{projectId} as getProject
+  GET projects/{projectId}/comments/{commentId} as getProjectComments
+  PUT projects/{projectId} as updateProject
+  GET projects/statistics
+"""
+
+type MyRoutes = IsakSky.RouteProvider<routes>
+
+let router = MyRoutes(
+              getProjectHandler = (fun projectId -> printfn "You asked for project %d" projectId),
+              getProjectCommentsHandler = (fun projectId commentId ->
+                                                                    printfn "You asked for project %d and comment %d" projectId commentId),
+              updateProjectHandler = (fun p -> printfn "Updated project %d" p),
+              // If you don't provide a route name, one will be computed for you
+              GET__projects_statisticsHandler = (fun () -> printfn "You asked for project statistics")
+             )
+
+let url = MyRoutes.getProjectComments(projectId = 123L, commentId = 4L).ToString() // "/projects/123/comments/4"
+
+let url2 = MyRoutes.GET__projects_statistics()
+
+router.DispatchRoute("GET", "/projects/123")
+router.DispatchRoute("GET", "/projects/123/comments/4/")
