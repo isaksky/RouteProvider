740a16ee72855d1f171231f9dcf340e511c6168e
Isak Sky <isak.sky@gmail.com>
2015-12-20 22:44:16 -0700

    Update sample assemblyinfo

--- src/RouteProvider.Samples/MVC/AssemblyInfo.fs
+++ src/RouteProvider.Samples/MVC/AssemblyInfo.fs
@@ -1,22 +1,12 @@
-module FSharpWeb3.AssemblyInfo
+﻿namespace System
 open System.Reflection
-open System.Runtime.CompilerServices
 
+[<assembly: AssemblyTitleAttribute("RouteProviderMVC")>]
+[<assembly: AssemblyProductAttribute("RouteProvider")>]
+[<assembly: AssemblyDescriptionAttribute("A type provider for web routing")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
 
-[<assembly: AssemblyTitle("FSharpWeb3")>]
-[<assembly: AssemblyDescription("")>]
-[<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("")>]
-[<assembly: AssemblyCopyright("")>]
-[<assembly: AssemblyTrademark("")>]
-
-// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
-
-[<assembly: AssemblyVersion("1.0.0.0")>]
-
-//[<assembly: AssemblyDelaySign(false)>]
-//[<assembly: AssemblyKeyFile("")>]
-
-()
-
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
