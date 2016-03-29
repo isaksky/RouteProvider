5da002a96fc7314f945d482c1a9227d2fbcea7a1
Isak Sky <isak.sky@gmail.com>
2016-03-29 00:44:36 -0600

    add Suave sample

---
+++ src/RouteProvider.Samples/WithSuave/WithSuave/WithSuave/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace WithSuave.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("WithSuave")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("WithSuave")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("b6ed5692-4619-47fb-9932-e1783e189d8e")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ src/RouteProvider.Samples/WithSuave/WithSuave/WithSuave/Program.fs
@@ -0,0 +1,59 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open Suave
+open Suave.Http
+open Suave.Operators
+open Suave.Successful
+open Suave.Logging
+
+open IsakSky
+
+type HttpMethod with
+  member this.Name =
+    match this with
+    | HttpMethod.GET -> "GET"
+    | HttpMethod.POST -> "POST"
+    | HttpMethod.DELETE -> "DELETE"
+    | HttpMethod.PUT -> "PUT"
+    | HttpMethod.HEAD -> "HEAD"
+    | HttpMethod.CONNECT -> "CONNECT"
+    | HttpMethod.PATCH -> "PATCH"
+    | HttpMethod.TRACE -> "TRACE"
+    | HttpMethod.OPTIONS -> "OPTIONS"
+    | HttpMethod.OTHER(name) -> name
+
+[<Literal>]
+let routes = """
+  GET projects/{projectId} as getProject
+  PUT projects/{projectId} as updateProject
+  GET projects/{projectName:string}/comments/{commentId} as getProjectComments
+"""
+
+[<Literal>]
+let outputPath = __SOURCE_DIRECTORY__ + "\MyRoutes.fs"
+
+type Dummy = RouteProvider<"MyRoutes", routes, true, true, outputPath>
+open MyNamespace.MyModule
+
+let router = {
+  getProject = fun (ctx:HttpContext) projectId ->
+    ctx |> OK(sprintf "Get project %d" projectId)
+  updateProject = fun ctx projectId ->
+    ctx |> OK(sprintf "Update project %d" projectId)
+  getProjectComments = fun ctx projectName commentId ->
+    ctx |> OK(sprintf "GET Project \"%s\" / Comment %d" projectName commentId)
+  notFound = Some <| (fun ctx verb path ->
+    (RequestErrors.NOT_FOUND "Not found") ctx)}
+
+let dispatchRouter : WebPart =
+  fun ctx ->
+    async {
+      return! router.DispatchRoute(ctx, ctx.request.``method``.Name, ctx.request.url)
+    }
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" argv
+    let cfg = { defaultConfig with logger = Loggers.ConsoleWindowLogger LogLevel.Verbose}
+    startWebServer defaultConfig dispatchRouter
+    0
