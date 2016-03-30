8840711958db154f4907c0d7317c0702d6b072c6
Isak Sky <isak.sky@gmail.com>
2016-03-29 21:28:45 -0600

    fix tests close #3

--- tests/RouteProvider.Tests/Tests.fs
+++ tests/RouteProvider.Tests/Tests.fs
@@ -10,20 +10,22 @@ let routesStr1 = """
   POST people/{personId}/status/{status:string}
 """
 [<Literal>]
-let outputPath = __SOURCE_DIRECTORY__ + "Routes.fs"
+let outputPath1 = __SOURCE_DIRECTORY__ + "\MyRoutes.fs"
 
-type Routes1 = IsakSky.RouteProvider<routesStr1, true, true, outputPath>
+type Routes1 = IsakSky.RouteProvider<"MyRoutes",routesStr1, false, true, outputPath1, "Ns1">
+open Ns1.MyModule
 
 [<Test>]
 let ``2part route`` () =
   let routesHit = Array.create 4 false
   let router1 =
-    Routes1(
-      GET__projects = (fun p -> routesHit.[0] <- true),
-      PUT__projects_action = (fun p -> routesHit.[1] <- true),
-      POST__people = (fun () -> routesHit.[2] <- true),
-      POST__people_status = (fun p s -> routesHit.[3] <- true)  
-    )
+    {
+      GET__projects = (fun p -> routesHit.[0] <- true)
+      PUT__projects_action = (fun p -> routesHit.[1] <- true)
+      POST__people = (fun () -> routesHit.[2] <- true)
+      POST__people_status = (fun p s -> routesHit.[3] <- true)
+      notFound = None
+    }
   router1.DispatchRoute("GET", "/projects/1")
   Assert.IsTrue(routesHit.[0])
   Assert.IsFalse(routesHit.[1])
@@ -34,12 +36,13 @@ let ``2part route`` () =
 let ``3part route`` () =
   let routesHit = Array.create 4 false
   let router1 =
-    Routes1(
-      GET__projects = (fun p -> routesHit.[0] <- true),
-      PUT__projects_action = (fun p -> routesHit.[1] <- true),
-      POST__people = (fun () -> routesHit.[2] <- true),
-      POST__people_status = (fun p s -> routesHit.[3] <- true)  
-    )
+    {
+      GET__projects = (fun p -> routesHit.[0] <- true)
+      PUT__projects_action = (fun p -> routesHit.[1] <- true)
+      POST__people = (fun () -> routesHit.[2] <- true)
+      POST__people_status = (fun p s -> routesHit.[3] <- true)
+      notFound = None
+    }
   router1.DispatchRoute("PUT", "/projects/1/action")
   Assert.IsFalse(routesHit.[0])
   Assert.IsTrue(routesHit.[1])
@@ -50,12 +53,13 @@ let ``3part route`` () =
 let ``4part route`` () =
   let mutable routeHit = false
   let router1 =
-    Routes1(
-      GET__projects = (fun p -> ()),
-      PUT__projects_action = (fun p -> ()),
-      POST__people = (fun () -> ()),
-      POST__people_status = (fun p s -> routeHit <- true)  
-    )
+    {
+      GET__projects = (fun p -> ())
+      PUT__projects_action = (fun p -> ())
+      POST__people = (fun () -> ())
+      POST__people_status = (fun p s -> routeHit <- true)
+      notFound = None
+    }
   router1.DispatchRoute("POST", "people/123/status/chilling")
   Assert.IsTrue(routeHit)
 
@@ -65,16 +69,21 @@ let routesStr2 = """
   GET projects/{action:string} as getProjAction
 """
 
-type Routes2 = IsakSky.RouteProvider<routesStr2>
+[<Literal>]
+let outputPath2 = __SOURCE_DIRECTORY__ + "\MyRoutes2.fs"
+
+type Routes2 = IsakSky.RouteProvider<"MyRoutes2",routesStr2, false, false, outputPath2, "Ns2">
+open Ns2.MyModule
 
 [<Test>]
 let ``const strings prioritized over dyn strings`` () =
   let mutable routeHit = false
   let router =
-    Routes2(
-      getProjStats = (fun () -> routeHit <- true),
+    {
+      MyRoutes2.getProjStats = (fun () -> routeHit <- true)
       getProjAction = (fun s -> ())
-    )
+      notFound = None
+    }
   router.DispatchRoute("GET", "projects/statistics")
   Assert.IsTrue(routeHit)
 
@@ -82,34 +91,38 @@ let ``const strings prioritized over dyn strings`` () =
 let ``dyn strings captured`` () =
   let mutable action = ""
   let router =
-    Routes2(
-      getProjStats = (fun () -> ()),
+    {
+      MyRoutes2.getProjStats = (fun () -> ())
       getProjAction = (fun routeAction -> action <- routeAction)
-    )
+      notFound = None
+    }
   router.DispatchRoute("GET", "projects/aggregate")
   Assert.AreEqual(action, "aggregate")
 
-type Routes3 = IsakSky.RouteProvider<routesStr2, "string">
+[<Literal>]
+let outputPath3 = __SOURCE_DIRECTORY__ + "\MyRoutes3.fs"
+type Routes3 = IsakSky.RouteProvider<"MyRoutes3", routesStr2, true, true, outputPath3, "Ns3">
+open Ns3.MyModule
 
 [<Test>]
 let ``input type passed in correctly`` () =
   let router =
-    Routes3(
-      getProjStats = (fun ctx -> Assert.AreEqual(ctx, "yep")),
+    {
+      MyRoutes3.getProjStats = (fun ctx -> Assert.AreEqual(ctx, "yep"))
       getProjAction = (fun ctx s -> ())
-    )
+      notFound = None
+    }
   router.DispatchRoute("yep", "GET", "projects/statistics")
-
-type Routes4 = IsakSky.RouteProvider<routesStr2, "", "string">
 
 [<Test>]
 let ``return type sent back`` () =
   let router =
-    Routes4(
-      getProjStats = (fun () -> ""),
-      getProjAction = (fun routeAction -> routeAction)
-    )
-  let resp = router.DispatchRoute("GET", "projects/archive")
+    {
+      MyRoutes3.getProjStats = (fun _ -> "")
+      getProjAction = (fun _ routeAction -> routeAction)
+      notFound = None
+    }
+  let resp = router.DispatchRoute((), "GET", "projects/archive")
   Assert.AreEqual(resp, "archive")
 
 
@@ -120,21 +133,24 @@ let routesStr4 = """
   GET projects/{action:string} as getProjectAction
 """
 
-type Routes5 = IsakSky.RouteProvider<routesStr4, "", "string">
+let [<Literal>] outputPath4 = __SOURCE_DIRECTORY__ + "\MyRoutes4.fs"
+type Routes4 = IsakSky.RouteProvider<"MyRoutes4", routesStr4, false, true, outputPath4, "Ns4">
+open Ns4.MyModule
 
 [<Test>]
 let ``conflicting routes ordering`` () =
   let router =
-    Routes5(
-      getProjectStats = (fun () -> "stats"),
-      getProject = (fun projectId -> sprintf "%d" projectId),
+    {
+      MyRoutes4.getProjectStats = (fun () -> "stats")
+      getProject = (fun projectId -> sprintf "%d" projectId)
       getProjectAction = (fun routeAction -> routeAction)
-    )
+      notFound = None
+    }
   let resp = router.DispatchRoute("GET", "/projects/statistics")
   Assert.AreEqual(resp, "stats")
-  
+
   let resp2 = router.DispatchRoute("GET", "/projects/archive")
   Assert.AreEqual(resp2, "archive")
-  
+
   let resp3 = router.DispatchRoute("GET", "/projects/1")
   Assert.AreEqual(resp3, "1")
