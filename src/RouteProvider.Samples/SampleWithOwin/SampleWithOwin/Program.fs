23d49ca47de243aeebbb2a7b89ddeed3c6d30537
Isak Sky <isak.sky@gmail.com>
2016-03-29 19:08:33 -0600

    update owin example

--- src/RouteProvider.Samples/SampleWithOwin/SampleWithOwin/Program.fs
+++ src/RouteProvider.Samples/SampleWithOwin/SampleWithOwin/Program.fs
@@ -6,41 +6,54 @@ open System.Threading.Tasks
 open Microsoft.Owin.Hosting
 open Microsoft.Owin.Host.HttpListener
 
+open IsakSky.RouteProvider
+
 [<Literal>]
 let routes = """
   GET  projects/{project}/comments/{comment} as getProjectComments
   POST projects/{project}/comments as putProjectComments
 """
 
-type Routes = IsakSky.RouteProvider<routes, "Microsoft.Owin.IOwinContext">
+let [<Literal>] outputPath = __SOURCE_DIRECTORY__ + "\MyRoutes.fs"
 
-let router = Routes(getProjectComments = (fun ctx projectId commentId -> 
-  ctx.Response.Write(sprintf "You asked for project %d and comment %d" projectId commentId)),
-                    putProjectComments = (fun ctx projectId -> 
-                                          ctx.Response.Write(sprintf "You asked for project %d" projectId)))
+open MyNamespace.MyModule
+
+type Dummy = IsakSky.RouteProvider<"MyRoutes", routes, true, true, outputPath>
+
+//let router = Routes(getProjectComments = (fun ctx projectId commentId ->
+//  ctx.Response.Write(sprintf "You asked for project %d and comment %d" projectId commentId)),
+//                    putProjectComments = (fun ctx projectId ->
+//                                          ctx.Response.Write(sprintf "You asked for project %d" projectId)))
+
+let router =
+  { getProjectComments = fun (ctx:IOwinContext) projectId commentId ->
+      ctx.Response.Write(sprintf "You asked for project %d and comment %d" projectId commentId)
+    putProjectComments = fun ctx projectId ->
+      ctx.Response.Write(sprintf "You asked for project %d" projectId)
+    notFound = None}
 
 type WebApp() =
   let handleOwinContext (ctx:IOwinContext) =
-    try 
+    try
       router.DispatchRoute(ctx, ctx.Request.Method, ctx.Request.Path.ToString())
-    with 
-    | :? Routes.RouteNotMatchedException as ex ->
-      printfn "Route %s %s not matched" ex.Verb ex.Path
- 
-  let owinHandler = fun (context:IOwinContext) (_:Func<Task>) -> 
+    with
+    | MyNamespace.MyModule.Internal.RouteNotMatchedException(verb, path) ->
+      printfn "Route %s %s not matched" verb path
+
+  let owinHandler = fun (context:IOwinContext) (_:Func<Task>) ->
     handleOwinContext context;
     Task.FromResult(null) :> Task;
 
-  member x.Configuration (app:IAppBuilder) = 
+  member x.Configuration (app:IAppBuilder) =
     app.Use(owinHandler) |> ignore
 
 [<assembly: OwinStartup(typeof<WebApp>)>]
 do ()
 
 [<EntryPoint>]
-let main argv = 
+let main argv =
     let url = "http://*:4567"
-    
+
     try
       printfn "Starting up..."
       use app = WebApp.Start<WebApp>(url)
